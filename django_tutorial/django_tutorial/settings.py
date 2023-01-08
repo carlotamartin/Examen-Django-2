67afb4d2e8f15a222ad369c03a985008a3e4ac34
carlotamartin <91574355+carlotamartin@users.noreply.github.com>
2023-01-08 09:29:41 +0100

    cambiando la base de datos

--- django_tutorial/django_tutorial/settings.py
+++ django_tutorial/django_tutorial/settings.py
@@ -74,10 +74,15 @@ WSGI_APPLICATION = 'django_tutorial.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'railway',
+        'USER': 'root',
+        'PASSWORD' : 'Dgwu6XP5THiQYEnYuyoK',
+        'HOST' : 'containers-us-west-140.railway.app',
+        'PORT' : '6131',
     }
 }
 
