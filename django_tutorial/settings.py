28f3e81f308e3b79b05560d436364b5a440ca79c
carlotamartin <91574355+carlotamartin@users.noreply.github.com>
2023-01-10 13:18:46 +0100

    cambiamos la base de datos a mysql

--- django_tutorial/settings.py
+++ django_tutorial/settings.py
@@ -43,7 +43,6 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
-    "whitenoise.middleware.WhiteNoiseMiddleware",
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -80,9 +79,9 @@ WSGI_APPLICATION = 'django_tutorial.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'Enter',
+        'NAME': 'examen',
         'USER': 'root',
-        'PASSWORD': 'uax',
+        'PASSWORD': '',
         'HOST': 'localhost',
         'PORT': '3306',
     }
